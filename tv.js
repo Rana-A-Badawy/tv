e8079b2cb77e37d9d26e52e1fb6b1c14099d59ab
Rana <ranaeng500238@gmail.com>
2024-10-06 12:12:36 +0300

    new

--- tv.js
+++ tv.js
@@ -1,39 +1,38 @@
-window.addEventListener('scroll', function() {
-    const navbar = document.getElementById('navbar');
-    const btn = document.getElementById('btn');  
-    if (window.scrollY > 0) {
-      navbar.classList.add('scrolled');
-    } else {
-      navbar.classList.remove('scrolled');
-    }
-    if (window.scrollY >= 200) {
-      btn.style.display = "block";
-    } else {
-      btn.style.display = "none";
-    }
-  });
-////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Toggle dark mode
-// Toggle dark mode
-const themeToggle = document.getElementById('theme-toggle');
+
+const themeToggle3 = document.getElementById('theme-toggle3');
+
 if (localStorage.getItem('theme') === 'dark') {
     document.body.classList.add('dark-mode');
 }
-themeToggle.addEventListener('click', () => {
+themeToggle3.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
-
     if (document.body.classList.contains('dark-mode')) {
         localStorage.setItem('theme', 'dark');
     } else {
         localStorage.setItem('theme', 'light');
     }
 });
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    const btn = document.getElementById('btn');
+
+    if (window.scrollY > 0) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+
+    if (window.scrollY >= 200) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
+});
+////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Toggle mobile menu
 const menuIcon = document.getElementById('menu-icon');
 const navLinks = document.getElementById('nav-links');
 menuIcon.addEventListener('click', () => {
- navLinks.classList.toggle('show');
+    navLinks.classList.toggle('show');
 });
-
