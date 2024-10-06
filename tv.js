490d559a078badd5f16806b76b23f0b50e99273d
omnia <Omniaabdelnasser067@gmail.com>
2024-10-06 11:12:35 +0300

    sdjhcv

--- tv.js
+++ tv.js
@@ -1,40 +1,41 @@
-window.addEventListener('scroll', function() {
-    const navbar = document.getElementById('navbar');
-    const btn = document.getElementById('btn');  
-    if (window.scrollY > 0) {
-      navbar.classList.add('scrolled');
-    } else {
-      navbar.classList.remove('scrolled');
-    }
-    if (window.scrollY >= 200) {
-      btn.style.display = "block";
-    } else {
-      btn.style.display = "none";
-    }
-  });
+// window.addEventListener('scroll', function() {
+//     const navbar = document.getElementById('navbar');
+//     const btn = document.getElementById('btn');  
+//     if (window.scrollY > 0) {
+//       navbar.classList.add('scrolled');
+//     } else {
+//       navbar.classList.remove('scrolled');
+//     }
+//     if (window.scrollY >= 200) {
+//       btn.style.display = "block";
+//     } else {
+//       btn.style.display = "none";
+//     }
+//   });
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Toggle dark mode
 // Toggle dark mode
-const themeToggle = document.getElementById('theme-toggle');
-if (localStorage.getItem('theme') === 'dark') {
-    document.body.classList.add('dark-mode');
-}
-themeToggle.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
+// const themeToggle = document.getElementById('theme-toggle');
+// if (localStorage.getItem('theme') === 'dark') {
+//     document.body.classList.add('dark-mode');
+// }
+// themeToggle.addEventListener('click', () => {
+//     document.body.classList.toggle('dark-mode');
 
-    if (document.body.classList.contains('dark-mode')) {
-        localStorage.setItem('theme', 'dark');
-    } else {
-        localStorage.setItem('theme', 'light');
-    }
-});
+//     if (document.body.classList.contains('dark-mode')) {
+//         localStorage.setItem('theme', 'dark');
+//     } else {
+//         localStorage.setItem('theme', 'light');
+//     }
+// });
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Toggle mobile menu
-const menuIcon = document.getElementById('menu-icon');
-const navLinks = document.getElementById('nav-links');
-menuIcon.addEventListener('click', () => {
- navLinks.classList.toggle('show');
-});
+// const menuIcon = document.getElementById('menu-icon');
+// const navLinks = document.getElementById('nav-links');
+// menuIcon.addEventListener('click', () => {
+//  navLinks.classList.toggle('show');
+// });
 
-    
+    //////////////////////////////////////////////////////////////////////////////////////
+ 
